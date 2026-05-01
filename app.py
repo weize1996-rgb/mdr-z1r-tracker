e38e31621f3972c109f40bfa43dbdd0040f77857
weize1996-rgb <weize1996@gmail.com>
2026-05-01 21:31:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from main import run
 
-app = Flask(__name__)
-
 @app.route("/")
 def home():
-    run()   # 🔥 這行是關鍵
+    print("🔥🔥🔥 ROUTE HIT 🔥🔥🔥")
+    run()
     return "OK"
