9e2d97649911627e41542437f5be055f93976833
weize1996-rgb <weize1996@gmail.com>
2026-05-01 21:38:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from sources.shopee import search_shopee
+from filter import is_valid, is_used, seen_before
 from notify import send_line
 
+TARGET = 30000
+
 def run():
-    print("🔥 run() 被執行了")
-    send_line("測試成功🔥")
+    items = search_shopee()
+
+    for item in items:
+        if not is_valid(item):
+            continue
+
+        if item["price"] < TARGET and not seen_before(item["id"]):
+            tag = "二手" if is_used(item) else "新品"
+
+            msg = f"""🔥 MDR-Z1R 降價（{tag}）
+💰 {item['price']}
+📝 {item['title']}
+🔗 {item['url']}"""
+
+            send_line(msg)
