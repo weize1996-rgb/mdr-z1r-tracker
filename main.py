7e54caabc56c1ea92041128c496f26dc6105fa29
weize1996-rgb <weize1996@gmail.com>
2026-05-02 10:22:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from filter import is_valid, is_used, seen_before
 from notify import send_line
 from ai_engine import update, score, is_scam, avg
 
-TARGET = 30000
+TARGET = 99999
 
 def run():
     print("🔥 run() START")
